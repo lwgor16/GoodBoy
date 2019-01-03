f903cab44080711c17e51e418340b87b25eaac56
lwgor16 <mykit.23@gmail.com>
2019-01-03 23:27:25 +0800

    Initial

--- app/src/main/java/my/edu/tarc/goodboy/HomeFragment.java
+++ app/src/main/java/my/edu/tarc/goodboy/HomeFragment.java
@@ -1,12 +1,14 @@
 package my.edu.tarc.goodboy;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 /**
@@ -14,18 +16,114 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
-    public HomeFragment() {
+    ViewPager viewPager, viewPager2;
+    ImageAdapter imageAdapter, imageAdapter2;
+    LinearLayout sliderDotspanel, sliderDotspanel2;
+    private int dotscount,dotscount2;
+    private ImageView[] dots, dots2;
+    public HomeFragment()
+    {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        viewPager2 = (ViewPager) view.findViewById(R.id.viewPager2);
 
+        sliderDotspanel =  (LinearLayout) view.findViewById(R.id.SliderDots);
+        sliderDotspanel2 = (LinearLayout) view.findViewById(R.id.SliderDots2);
+
+        imageAdapter = new ImageAdapter(getActivity());
+        imageAdapter2 = new ImageAdapter(getActivity());
+
+        viewPager.setAdapter(imageAdapter);
+        viewPager2.setAdapter(imageAdapter2);
+
+        dotscount = imageAdapter.getCount();
+        dotscount2 = imageAdapter2.getCount();
+
+        dots = new ImageView[dotscount];
+        dots2 = new ImageView[dotscount2];
+
+        for(int i = 0;i < dotscount;i++)
+        {
+            dots[i] = new ImageView(getActivity());
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(),R.drawable.nonactive_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8,0,8,0);
+
+            sliderDotspanel.addView(dots[i],params);
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(),R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+                for(int x = 0;x<dotscount;x++)
+                {
+                    dots[x].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(),R.drawable.nonactive_dot));
+                }
+                dots[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(),R.drawable.active_dot));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        for(int i = 0;i < dotscount2;i++)
+        {
+            dots2[i] = new ImageView(getActivity());
+            dots2[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(),R.drawable.nonactive_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8,0,8,0);
+
+            sliderDotspanel2.addView(dots2[i],params);
+        }
+
+        dots2[0].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(),R.drawable.active_dot));
+
+        viewPager2.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+                for(int x = 0;x<dotscount;x++)
+                {
+                    dots2[x].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(),R.drawable.nonactive_dot));
+                }
+                dots2[i].setImageDrawable(ContextCompat.getDrawable(getActivity().getApplicationContext(),R.drawable.active_dot));
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
         return view;
     }
+
+
 }
---
+++ app/src/main/java/my/edu/tarc/goodboy/ImageAdapter.java
@@ -0,0 +1,41 @@
+package my.edu.tarc.goodboy;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class ImageAdapter extends PagerAdapter {
+    private Context mContext;
+    private int[] mImageIds = new int[]{R.drawable.bye,R.drawable.gg,R.drawable.hi};
+
+    ImageAdapter(Context context)
+    {
+        mContext = context;
+    }
+    @Override
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view,  Object o) {
+        return view == o;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(mContext);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setImageResource(mImageIds[position]);
+        container.addView(imageView,0);
+        return imageView;
+
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position,  Object object) {
+        container.removeView((ImageView) object);
+    }
+}
